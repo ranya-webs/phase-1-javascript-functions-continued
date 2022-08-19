dc4c588c3e6d2f8516f2c7d02d7984008eda6bbb
Ranya Al-Jumaili <ranyaosam1@gmail.com>
2022-08-19 18:58:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// code your solution here
+/*
+ * This function returns the default activity 'roller-skate' when no arguments are passed. 
+ * It permits the default activity to be overriden
+ */
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+
+/*
+ * uses the default activity 'go to the office' when no arguments are passed, and permits the default
+ activity to be overriden. 
+ */
+function mondayWork(defaultActivity = "go to the office") {
+    return `This Monday, I will ${defaultActivity}.`;
+}
+mondayWork();
+mondayWork("work from home")
+
+
+/*
+when initialized with a symbol, e.g.'*', this function creates a function that, when called, wraps 
+an adjective in a highlight
+*/
+function wrapAdjective(symbol) {
+    return text => `You are ${symbol}${text}${symbol}!`;
+}
+wrapAdjective("*");
+wrapAdjective("||");
